b96dd57fab1316c979fa090bebd3dd6c6e956665
park <chanyoung990704@gmail.com>
2025-12-05 14:43:15 +0900

    gradle

---
+++ src/main/java/com/aivle06/bookservice/dto/BookRequestDTO.java
@@ -0,0 +1,17 @@
+package com.aivle06.bookservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookRequestDTO {
+
+    // 책 등록
+    static class c
+
+}
