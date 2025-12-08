696f4aac4dd42c52b1b29221d208ad2068babaa7
AwkAwkAardvark <a081127@naver.com>
2025-12-08 14:06:34 +0900

    feat/Add User and reflects M:1 in Book (#20)

---
+++ src/main/java/com/aivle06/bookservice/domain/User.java
@@ -0,0 +1,38 @@
+package com.aivle06.bookservice.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name= "user_id")
+    private Long id;
+
+    private String name;
+
+    @CreationTimestamp
+    private LocalDateTime member_since;
+
+    @OneToMany(mappedBy = "user")
+    @JsonManagedReference
+    private List<Book> books = new ArrayList<>();
+    // getBooks()하다가 null 생길까봐 일단 빈 List생성
+
+
+}
