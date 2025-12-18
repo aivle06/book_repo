393d19818e90ba260213a6f28f98828869e1674c
park <chanyoung990704@gmail.com>
2025-12-18 16:00:27 +0900

    modf sh4

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ cd /home/ec2-user/app
 # ... (기존 종료 로직 유지) ...
 
 # [수정된 부분] 폴더 구조가 유지되므로 build/libs/ 안까지 깊게 찾도록 변경
-JAR_NAME=$(find . -name "*.jar" | tail -n 1)
+JAR_NAME=$(find . -name "*.jar" ! -name "*plain.jar" | tail -n 1)
 
 echo "> 새 애플리케이션 배포: $JAR_NAME"
 
