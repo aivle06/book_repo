0f14f88a62ef8f75b322614dd179d2d961de6e0c
park <chanyoung990704@gmail.com>
2025-12-18 15:42:06 +0900

    modf sh3

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-# 배포 디렉토리로 이동
 cd /home/ec2-user/app
 
-# 실행 중인 애플리케이션 종료 (기존 프로세스가 있다면)
-CURRENT_PID=$(pgrep -f .jar)
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
+# ... (기존 종료 로직 유지) ...
 
-# 새 JAR 파일 찾기 및 실행
-JAR_NAME=$(ls -tr *.jar | tail -n 1)
+# [수정된 부분] 폴더 구조가 유지되므로 build/libs/ 안까지 깊게 찾도록 변경
+JAR_NAME=$(find . -name "*.jar" | tail -n 1)
+
 echo "> 새 애플리케이션 배포: $JAR_NAME"
 
-# [수정된 부분] 표준 입력을 /dev/null로 리다이렉트하여 CodeDeploy 연결 해제
+# [기존 수정사항 유지] 표준 입력 닫기 적용
 nohup java -jar $JAR_NAME > /home/ec2-user/app/nohup.out 2>&1 < /dev/null &
