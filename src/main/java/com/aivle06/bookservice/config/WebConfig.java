cc3732dc3c6f73263774a6ab05f17f6f6db9c6ff
park <chanyoung990704@gmail.com>
2025-12-23 13:17:17 +0900

    Update FrontEndPoint2

--- src/main/java/com/aivle06/bookservice/config/WebConfig.java
+++ src/main/java/com/aivle06/bookservice/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("Guna-LB-1989735834.us-east-2.elb.amazonaws.com")
+                .allowedOrigins("http://guna-lb-1989735834.us-east-2.elb.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
